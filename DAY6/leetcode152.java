1313c54abb516bbb059839dfac33ae9e0bcf34bd
Gautam Singh <147837305+PANDA1506@users.noreply.github.com>
2025-10-16 15:32:42 +0530

    Create leetcode152.java

---
+++ DAY6/leetcode152.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int rightprod = 1;
+        int leftprod = 1;
+        int ans = nums[0];
+
+        for (int i = 0; i < n; i++){
+
+            leftprod = leftprod == 0 ? 1 :leftprod;
+            rightprod = rightprod == 0 ? 1 :rightprod;
+
+            leftprod *= nums[i];
+            rightprod *= nums[n-1-i];
+
+            ans = Math.max(ans, Math.max(leftprod, rightprod));
+        }
+        return ans;
+    }
+}
