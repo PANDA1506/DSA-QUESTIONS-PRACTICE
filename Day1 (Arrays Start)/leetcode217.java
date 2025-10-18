04ffff86e022c52741d19625195d8defee9f0963
Gautam Singh <147837305+PANDA1506@users.noreply.github.com>
2025-10-18 17:52:32 +0530

    Create leetcode217.java

---
+++ Day1 (Arrays Start)/leetcode217.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> sets = new HashSet<>();
+
+        for(int num : nums){
+            if (sets.contains(num))
+                return true;
+            sets.add(num);
+        }
+       return false;
+    }
+}
