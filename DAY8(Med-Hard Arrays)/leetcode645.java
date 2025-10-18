527fe653cf32c0d5adf106f7c46a1480e46cba55
Gautam Singh <147837305+PANDA1506@users.noreply.github.com>
2025-10-19 00:25:27 +0530

    Update leetcode645.java

--- DAY8(Med-Hard Arrays)/leetcode645.java
+++ DAY8(Med-Hard Arrays)/leetcode645.java
@@ -1,1 +1,22 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int duplicate = -1;
+        int actualSum = 0;
+        int n = nums.length;
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                duplicate = num; 
+            }
+            set.add(num);
+            actualSum += num;
+        }
+
+        int expectedSum = n * (n + 1) / 2;
+        int missing = expectedSum - (actualSum - duplicate);
+
+        return new int[]{duplicate, missing};
+    }
+}
 
